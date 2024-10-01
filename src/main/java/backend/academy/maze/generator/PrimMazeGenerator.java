66724a86442c978dd251902f6227a4dde7064603
vodoem <prosto_duz@mail.ru>
2024-10-01 22:32:28 +0300

    Добавлены класс, который в будующем будет реализовывыать генерацию лабиринта алгоритмом Прима

---
+++ src/main/java/backend/academy/maze/generator/PrimMazeGenerator.java
@@ -0,0 +1,12 @@
+package backend.academy.maze.generator;
+
+import backend.academy.maze.data.Maze;
+
+public class PrimMazeGenerator implements Generator {
+    @Override
+    public Maze generate(int height, int width) {
+        // Реализация алгоритма Прима для генерации лабиринта
+        //return new Maze(height, width, grid);
+        return null;
+    }
+}
