18950ad569f2a453a46b0c913ac65c422d00fd34
vodoem <prosto_duz@mail.ru>
2024-10-01 22:32:15 +0300

    Добавлены класс, который в будующем будет реализовывыать генерацию лабиринта алгоритмом Краскала

---
+++ src/main/java/backend/academy/maze/generator/KruskalMazeGenerator.java
@@ -0,0 +1,14 @@
+package backend.academy.maze.generator;
+
+import backend.academy.maze.data.Maze;
+
+public class KruskalMazeGenerator implements Generator {
+
+    @Override
+    public Maze generate(int height, int width) {
+        // Реализация алгоритма Краскала для генерации лабиринта
+
+        //return new Maze(height, width, grid);
+        return null;
+    }
+}
