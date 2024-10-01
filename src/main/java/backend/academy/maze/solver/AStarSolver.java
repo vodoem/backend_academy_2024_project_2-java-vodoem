eb9312d2d5b73d81f2bb0ae06f32f040677071bd
vodoem <prosto_duz@mail.ru>
2024-10-01 22:30:06 +0300

    Добавлены класс, который в будующем будет реализовывыать решение лабиринта методом A*

---
+++ src/main/java/backend/academy/maze/solver/AStarSolver.java
@@ -0,0 +1,14 @@
+package backend.academy.maze.solver;
+
+import backend.academy.maze.data.Coordinate;
+import backend.academy.maze.data.Maze;
+import java.util.List;
+
+public class AStarSolver implements Solver {
+    @Override
+    public List<Coordinate> solve(Maze maze, Coordinate start, Coordinate end) {
+        // Реализация A* с использованием эвристики для нахождения оптимального пути
+        //return path;
+        return List.of();
+    }
+}
