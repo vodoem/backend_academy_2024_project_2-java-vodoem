556f312ea1674b835a1e0dec424dda4ca0c2c74f
vodoem <prosto_duz@mail.ru>
2024-10-01 22:38:09 +0300

    Были выполнены требования чекстайла

--- src/main/java/backend/academy/maze/renderer/Renderer.java
+++ src/main/java/backend/academy/maze/renderer/Renderer.java
@@ -2,7 +2,6 @@ package backend.academy.maze.renderer;
 
 import backend.academy.maze.data.Coordinate;
 import backend.academy.maze.data.Maze;
-
 import java.util.List;
 
 public interface Renderer {
--- src/main/java/backend/academy/maze/solver/Solver.java
+++ src/main/java/backend/academy/maze/solver/Solver.java
@@ -2,7 +2,6 @@ package backend.academy.maze.solver;
 
 import backend.academy.maze.data.Coordinate;
 import backend.academy.maze.data.Maze;
-
 import java.util.List;
 
 public interface Solver {
