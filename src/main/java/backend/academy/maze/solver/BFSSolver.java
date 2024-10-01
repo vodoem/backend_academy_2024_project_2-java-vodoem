5cd1cad886e1a7afba5d1633454c451ef9169beb
vodoem <prosto_duz@mail.ru>
2024-10-01 22:31:19 +0300

    Добавлены класс, который в будующем будет реализовывыать решение лабиринта используя поиск в ширину

---
+++ src/main/java/backend/academy/maze/solver/BFSSolver.java
@@ -0,0 +1,14 @@
+package backend.academy.maze.solver;
+
+import backend.academy.maze.data.Coordinate;
+import backend.academy.maze.data.Maze;
+import java.util.List;
+
+public class BFSSolver implements Solver {
+    @Override
+    public List<Coordinate> solve(Maze maze, Coordinate start, Coordinate end) {
+        // Реализация BFS для нахождения кратчайшего пути
+        //return path;
+        return List.of();
+    }
+}
